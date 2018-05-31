f1b68968034d14415698e406e6e5429e4809e65a
gracki <gracki@gracki.gracul>
2018-05-31 14:20:03 +0100

    improved post_detail

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,10 @@ from django.utils import timezone
 class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE, default="")
     title = models.CharField(max_length=200)
+    description = models.TextField(default="Two-three sentences as a description")
     text = models.TextField()
+    text_two = models.TextField(default="N/A")
+    text_three = models.TextField(blank=True)
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
     image_one = models.ImageField(default='image.png', blank=True)
